84a4617c7c055c36f71c66d924afef700b73898c
KyeNormanGill <kye.normangill@tafensw.net.au>
2017-05-21 18:45:00 +1000

    Comments :D

--- src/structures/client.js
+++ src/structures/client.js
@@ -6,56 +6,33 @@ const messageH = new MessageHandler();
 
 class SmoothClient extends Client {
 	/**
-	 * @typedef {Object} ClientOptions
-	 * @property {string} owner - The owners ID of this bot.
+	 * @typedef {Object} CommandOptions
+	 * @property {string} owner - The bot owners ID.
 	 * @property {string} prefix - The prefix to use with this bot.
-	 * @property {boolean} [autoAliases=true] - Whether automatic aliases should be added
-	 * @property {string} group - The ID of the group the command belongs to (must be lowercase)
-	 * @property {string} memberName - The member name of the command in the group (must be lowercase)
-	 * @property {string} description - A short description of the command
-	 * @property {string} [format] - The command usage format string - will be automatically generated if not specified,
-	 * and `args` is specified
-	 * @property {string} [details] - A detailed description of the command and its functionality
-	 * @property {string[]} [examples] - Usage examples of the command
-	 * @property {boolean} [guildOnly=false] - Whether or not the command should only function in a guild channel
-	 * @property {ThrottlingOptions} [throttling] - Options for throttling usages of the command.
-	 * @property {PermissionResolvable[]} [clientPermissions] - Permissions the bot needs for the command to function.
-	 * @property {PermissionResolvable[]} [userPermissions] - Permissions that a user must have to use the command.
-	 * @property {boolean} [defaultHandling=true] - Whether or not the default command handling should be used.
-	 * If false, then only patterns will trigger the command.
-	 * @property {ArgumentInfo[]} [args] - Arguments for the command.
-	 * @property {number} [argsPromptLimit=Infinity] - Maximum number of times to prompt a user for a single argument.
-	 * Only applicable if `args` is specified.
-	 * @property {string} [argsType=single] - One of 'single' or 'multiple'. Only applicable if `args` is not specified.
-	 * When 'single', the entire argument string will be passed to run as one argument.
-	 * When 'multiple', it will be passed as multiple arguments.
-	 * @property {number} [argsCount=0] - The number of arguments to parse from the command string.
-	 * Only applicable when argsType is 'multiple'. If nonzero, it should be at least 2.
-	 * When this is 0, the command argument string will be split into as many arguments as it can be.
-	 * When nonzero, it will be split into a maximum of this number of arguments.
-	 * @property {boolean} [argsSingleQuotes=true] - Whether or not single quotes should be allowed to box-in arguments
-	 * in the command string.
-	 * @property {RegExp[]} [patterns] - Patterns to use for triggering the command
-	 * @property {boolean} [guarded=false] - Whether the command should be protected from disabling
+	 * @property {boolean} selfbot - Whether or not this bot is a selfbot.
+	 * @property {string} commandDirectory - The directory in which the commands are held.
+	 * @property {boolean} debug - Whether or not to log extra information to console.
+	 * @property {boolean} unkownCommandNotification - If the bot should respond if it sees a message that starts with it's prefix but isn't a command.
 	 */
 
 	/**
-	 * @param {ClientOptions} options - The clients options
+	 * @param {ClientOptions} options - The clients options.
 	 */
 	constructor(options = {}) {
 		super(options);
 
-		if (options.commandDirectory === undefined) throw Error('No commands directory specified');
+		if (options.owner === undefined) throw Error('Could not find owner ID');
 		if (options.prefix === undefined) options.prefix = '!';
-		if (options.debug === undefined) options.debug = false;
 		if (options.selfbot === undefined) options.selfbot = false;
+		if (options.commandDirectory === undefined) throw Error('No commands directory specified');
+		if (options.debug === undefined) options.debug = false;
 		if (options.unkownCommandNotification === undefined) options.unkownCommandNotification = false;
 
 		/**
-		 * The directory of the commands.
+		 * The bot owners ID.
 		 * @type {string}
 		 */
-		this.commandDirectory = options.commandDirectory;
+		this.owner = options.owner;
 
 		/**
 		 * The prefix of the bot.
@@ -64,16 +41,22 @@ class SmoothClient extends Client {
 		this.prefix = options.prefix;
 
 		/**
-		 * Whether or not to log extra information to console.
-		 * @type {boolean}
-		 */
-		this.debug = options.debug;
-
-		/**
 		 * Whether or not this bot will be a selfbot.
 		 * @type {boolean}
 		 */
 		this.selfbot = options.selfbot;
+
+		/**
+		 * The directory of the commands.
+		 * @type {string}
+		 */
+		this.commandDirectory = options.commandDirectory;
+
+		/**
+		 * Whether or not to log extra information to console.
+		 * @type {boolean}
+		 */
+		this.debug = options.debug;
 
 		/**
 		 * If the bot should respond if it sees a message that starts with it's prefix but isn't a command.
--- src/structures/command.js
+++ src/structures/command.js
@@ -1,12 +1,36 @@
 
 class Command {
+	/**
+	 * @typedef {Object} ClientOptions
+	 * @property {string} name - The name of the command.
+	 * @property {string} description - The description of the command.
+	 * @property {boolean} guildOnly - If the command can only by ran inside a guild channel.
+	 */
+
+	/**
+	 * @param {CommandOptions} options - The commands options.
+	 */
 	constructor(options = {}) {
 		if (options.name === undefined) throw Error('No name property detected in command.');
 		if (options.description === undefined) throw Error('No description detected in command.');
 		if (options.guildOnly === undefined) options.guildOnly = false;
 
+		/**
+		 * The name of the command.
+		 * @type {string}
+		 */
 		this.name = options.name;
+
+		/**
+		 * The description of the command.
+		 * @type {string}
+		 */
 		this.description = options.description;
+
+		/**
+		 * If the command can only by ran inside a guild channel.
+		 * @type {boolean}
+		 */
 		this.guildOnly = options.guildOnly;
 	}
 }

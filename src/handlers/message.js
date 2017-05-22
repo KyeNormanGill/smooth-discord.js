a0968b0af3f9fe69e01a24dfe012d2b74b3902e5
KyeNormanGill <kye.normangill@tafensw.net.au>
2017-05-22 17:38:16 +1000

    stuff

--- src/handlers/message.js
+++ src/handlers/message.js
@@ -2,35 +2,40 @@ class Handler {
 	handleMessage(message) {
 		const prefix = message.client.prefix;
 
-		// Prefix check
+		// Prefix check.
 		if (!message.content.startsWith(prefix)) return;
 
-		// Required checks
+		// Required checks.
 		if (message.author.bot) return;
 
-		// Selfbot check
+		// Selfbot check.
 		if (message.client.selfbot && message.author.id !== message.client.owner) return;
 
-		// Get Command
+		// Get Command.
 		const commandName = message.content.slice(prefix.length).split(' ')[0].toLowerCase();
 		const command = message.client.commands.get(commandName)
 			|| message.client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
 
-		// Command check
+		// Command check.
 		if (!command) {
 			if (message.client.unkownCommandResponse) message.reply('Unkown command.');
 			return;
 		}
 
-		// GuildOnly Check
+		// GuildOnly Check.
 		if (command.guildOnly && message.channel.type !== 'text') return;
 
-		// Clean check
+		// Clean check.
 		if (command.clean) message.delete();
 
 		const args = message.content.split(' ').slice(1).join(' ');
+
+		// Run command.
 		command.run(message, args);
+
 		if (message.client.debug) console.log(`Command ${command.name} was triggered by ${message.author.username} with args: '${args}'`);
+
+		// Emit commandRun event
 		message.client.emit('commandRun', command);
 	}
 }

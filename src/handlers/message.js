35fb17b4a64de1cec13783ed31d98b538ec4ea89
KyeNormanGill <kye.normangill@tafensw.net.au>
2017-05-23 18:00:04 +1000

    besides that

--- src/handlers/message.js
+++ src/handlers/message.js
@@ -44,7 +44,7 @@ class Handler {
 			message.client.emit('commandError', command, err);
 
 			// Error response check
-			if (message.client.errorResponse) message.channel.send(`An error occured while trying to run **${command.name}.**\`\`\`${err.name}: ${err.message}\`\`\`Please contact: **${message.client.owners.map(owner => message.client.users.get(owner)).join(' ')}**`);
+			if (message.client.errorResponse) message.channel.send(`An error occured while trying to run **${command.name}.**\`\`\`${err.name}: ${err.message}\`\`\`Please contact: **${message.client.owners.map(owner => message.client.users.get(owner).tag).join(' ')}**`);
 		}
 	}
 }
